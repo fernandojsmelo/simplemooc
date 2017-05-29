eda7fea12b8b5d72b46762dea12e132c9d4b8094
fernando-not <fernandojsmelo@gmail.com>
2017-05-28 23:49:21 -0300

    Listagem dos Cursos

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from .models import Course
 # Create your views here.
 
 def index(request):
+    course = Course.object.all()
     template_name = 'courses/index.html'
-    return render(request, template_name)
+    context = {
+        'courses': course
+    }
+    return render(request, template_name, context)
