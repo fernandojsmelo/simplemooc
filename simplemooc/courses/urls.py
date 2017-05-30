6dc86206dfcfd8a0cf3bc545d187c59f1809180a
fernando-not <fernandojsmelo@gmail.com>
2017-05-29 21:08:28 -0300

    usando slug no curso

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$',views.index, name='index'),
-    url(r'^(?P<pk>\d+)/$',views.details, name='details'),
+    #url(r'^(?P<pk>\d+)/$',views.details, name='details'),
+    url(r'^(?P<slug>[\w_-]+)/$',views.details, name='details'),
 
 ]
