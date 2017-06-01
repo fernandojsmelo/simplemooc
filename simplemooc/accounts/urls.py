5e7a0de817aaf9c03796eddc76c17014d02099db
fernando-not <fernandojsmelo@gmail.com>
2017-05-31 23:15:02 -0300

    View de Logout

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -6,6 +6,8 @@ from simplemooc.accounts import views
 urlpatterns = [
     url(r'^entrar/$', auth_views.login,
         {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^sair/$', auth_views.logout,
+        {'next_page': 'core:home'}, name='logout'),
     url(r'^cadastre-se/$', views.register, name='register'),
 
 ]
