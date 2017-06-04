8f5fff7610a13cf7b3ee28283c757363a2c62393
fernando-not <fernandojsmelo@gmail.com>
2017-06-03 23:36:10 -0300

    Introdução a Criação de Nova Senha

--- simplemooc/accounts/models.py
+++ simplemooc/accounts/models.py
@@ -1,5 +1,6 @@
 import re
 
+from django.conf import settings
 from django.db import models
 from django.core import validators
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
@@ -40,3 +41,20 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
 
+class PasswordReset(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name='Usuário',
+        #related_name='resets'
+    )
+    key = models.CharField('Chave', max_length=100, unique=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    confirmed = models.BooleanField('Confirmado', default=False, blank=True)
+
+    def __str__(self):
+        return '{0} em {1}'.format(self.user, self.created_at)
+
+    class Meta:
+        verbose_name = 'Nova Senha'
+        verbose_name_plural = 'Novas Senhas'
+        ordering = ['-created_at']
