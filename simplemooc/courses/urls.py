f1d07413491ca8eb9f25f81e437279021dfdad57
fernando-not <fernandojsmelo@gmail.com>
2017-06-09 23:04:13 -0300

    Página do Anúncio e Comentários

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^(?P<slug>[\w_-]+)/cancelar-inscricao/$',views.undo_enrollment,
         name='undo_enrollment'),
     url(r'^(?P<slug>[\w_-]+)/anucios/$',views.announcements, name='announcements'),
+    url(r'^(?P<slug>[\w_-]+)/anucios/(?P<pk>\d+)/$',views.show_announcement, name='show_announcement'),
 
 ]
