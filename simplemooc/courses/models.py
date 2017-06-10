b3934fd373356797b4582c4d364ea4ae9d20d731
fernando-not <fernandojsmelo@gmail.com>
2017-06-09 21:12:30 -0300

    Modelagem e Admin dos Anúncios

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -79,3 +79,36 @@ class Enrollment(models.Model):
         verbose_name = 'Inscrição'
         verbose_name_plural = 'Inscrições'
         unique_together = (('user', 'course'),)
+
+class Announcement(models.Model):
+
+    course = models.ForeignKey(Course, verbose_name='Curso')
+    titel = models.CharField('Titulo', max_length=100)
+    content = models.TextField('Conteúdo')
+
+    created_at = models.DateTimeField( 'Criado em', auto_now_add=True )
+    updated_at = models.DateTimeField( 'Atualizado em', auto_now=True )
+
+    def __str__(self):
+        return self.titel
+
+    class Meta:
+        verbose_name = 'Anúcio'
+        verbose_name_plural = 'Anúcios'
+        ordering = ['-created_at']
+
+class Comment(models.Model):
+
+    announcement = models.ForeignKey(
+        Announcement, verbose_name='Anúcio', related_name='comments'
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='usuário')
+    comment = models.TextField('Comentário')
+
+    created_at = models.DateTimeField( 'Criado em', auto_now_add=True )
+    updated_at = models.DateTimeField( 'Atualizado em', auto_now=True )
+
+    class Meta:
+        verbose_name = 'Comentário'
+        verbose_name_plural = 'Comentários'
+        ordering = ['created_at']
