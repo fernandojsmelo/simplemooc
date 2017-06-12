08dd365b0671d100cffeac2edef19af6ca3cf85d
fernando-not <fernandojsmelo@gmail.com>
2017-06-12 16:06:19 -0300

    Modelagem das Aulas

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -48,6 +48,45 @@ class Course(models.Model):
         verbose_name_plural = 'Cursos'
         ordering = ['name']
 
+class Lesson(models.Model):
+
+    name = models.CharField('Nome', max_length=100)
+    description = models.TextField('Descrição', blank=True)
+    number = models.IntegerField('Numero (ordem)', blank=True, default=0)
+    release_date = models.DateField('Data de Liberação', blank=True, null=True)
+
+    course = models.ForeignKey(Course, verbose_name='Curso', related_name='lessons')
+
+    created_at = models.DateTimeField( 'Criado em', auto_now_add=True )
+    updated_at = models.DateTimeField( 'Atualizado em', auto_now=True )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Aula'
+        verbose_name_plural = 'Aulas'
+        ordering = ['number']
+
+class Material(models.Model):
+
+    name = models.CharField( 'Nome', max_length=100 )
+    embedded = models.TextField('Vido embedded', blank=True)
+    file = models.FileField(upload_to='lessons/materials', blank=True, null=True)
+
+    lesson = models.ForeignKey(Lesson, verbose_name='Aula', related_name='materials')
+
+    def is_embedded(self):
+        return bool(self.embedded)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Matérial'
+        verbose_name_plural = 'Materiais'
+
+
 class Enrollment(models.Model):
 
     STATUS_CHOICES = (
@@ -134,3 +173,4 @@ models.signals.post_save.connect(
     post_save_announcement, sender=Announcement,
     dispatch_uid='post_save_announcement'
 )
+
