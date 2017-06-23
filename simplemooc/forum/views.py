3066a5d44df42802b9b652fed307caf74023e3cc
fernando-not <fernandojsmelo@gmail.com>
2017-06-22 23:37:11 -0300

    Listagem dos Tópicos 2

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -17,7 +17,12 @@ class ForumView(TemplateView):
 class ForumView(ListView):
 
     model = Thread
-    paginate_by = 10
+    paginate_by = 2
     template_name = 'forum/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ForumView, self).get_context_data(**kwargs)
+        context['tags'] = Thread.tags.all()
+        return context
 
 index = ForumView.as_view()
