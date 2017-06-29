d42d3b54e8e9cd88a4b71431c9e22beb6776bef3
fernando-not <fernandojsmelo@gmail.com>
2017-06-28 23:29:23 -0300

    Resposta correta via ajax

--- simplemooc/forum/urls.py
+++ simplemooc/forum/urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$',views.index, name='index'),
-    url(r'^tag/(?P<tag>[\w_-]+)/$',views.index, name='index_tagged'),
-    url(r'^(?P<slug>[\w_-]+)/$',views.thread, name='thread'),
+    url(r'^tag/(?P<tag>[\w_-]+)/$', views.index, name='index_tagged'),
+    url( r'^resposta/(?P<pk>[\w_-]+)/correta/$', views.reply_correct, name='reply_correct' ),
+    url( r'^resposta/(?P<pk>[\w_-]+)/incorreta/$',views.reply_incorrect,name='inreply_correct' ),
+    url(r'^(?P<slug>[\w_-]+)/$', views.thread, name='thread'),
     ]
